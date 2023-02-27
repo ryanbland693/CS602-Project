8bc7044a8df9adc6eca431c5f57a84ee453d3ec0
ryanbland693 <ryanbland693@gmail.com>
2023-02-27 19:52:49 +0100

    removed console.log

--- routes/Admin/Exhibitions/deleteExhibition.js
+++ routes/Admin/Exhibitions/deleteExhibition.js
@@ -2,7 +2,6 @@ const db = require('../../../db')
 const ErrorHandler = require('../../../classes/ErrorHandler')
 
 module.exports = async (req, res, next) => {
-    console.log(req.params.id)
     db.query('CALL DeleteExhibition(?)', [req.params.id], (err, result, fields) => {
         if (err) {
             return next(new ErrorHandler(500).getError())
